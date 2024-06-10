fed3d166822e27a2b858cb2a1a6ee2c933fb189c
Minu <63788935+minuscholastica@users.noreply.github.com>
2024-06-10 21:50:14 +0900

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+      ...require("nextra")({
+        theme: "nextra-theme-docs",
+        themeConfig: "./theme.config.jsx",
+        latex: true,
+        titleSuffix:
+        "최강 패밀리의 AI 여정",
+      })()
+    };
